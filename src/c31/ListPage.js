6f89951b1cb0eb3c235e6627fb11516dbf3843bc
zhangyue2 <40255722+zhangyue2@users.noreply.github.com>
2019-07-31 11:37:46 +0800

    Initial commit

---
+++ src/c31/ListPage.js
@@ -0,0 +1,131 @@
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { Pagination, Table, Input } from "antd";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import { fetchList } from "./actions";
+import { createSelector } from "reselect";
+
+const getItems = state => state.items;
+const getById = state => state.byId;
+
+const dataSourceSelector = createSelector(getItems, getById, (items, byId) => {
+  console.log("reselect: get data source");
+  if (!items) return [];
+  return items.map(id => byId[id]);
+});
+
+class ListPage extends React.Component {
+  state = { search: "" };
+  componentDidMount() {
+    const page = this.props.match.params.page || 1;
+    if (
+      page !== this.props.list.page ||
+      !this.getDataSource(this.props.list, this.props.list.byId).length ||
+      this.props.list.needReloadList
+    )
+      this.fetchData(parseInt(page, 10));
+  }
+
+  componentDidUpdate(prevProps) {
+    const page = parseInt(this.props.match.params.page || 1, 10);
+    const prevPage = parseInt(prevProps.match.params.page || 1, 10);
+    if (prevPage !== page && !this.props.list.fetchListPending) {
+      this.fetchData(page);
+    }
+  }
+
+  fetchData(page) {
+    this.props.fetchList(page);
+  }
+
+  getDataSource = dataSourceSelector;
+
+  // getDataSource() {
+  //   console.log("get data source", this.props.list);
+  //   const { items, byId } = this.props.list;
+  //   if (!items) return [];
+  //   return items.map(id => byId[id]);
+  // }
+  getColumns() {
+    return [
+      {
+        title: "First Name",
+        dataIndex: "first_name",
+        key: "first_name",
+        width: "200px",
+        render: (firstName, rec) => (
+          <Link to={`/user/${rec.id}`}>{firstName}</Link>
+        ),
+      },
+      {
+        title: "Last Name",
+        dataIndex: "last_name",
+        key: "last_name",
+        width: "200px",
+      },
+    ];
+  }
+  handleSearch = keyword => {
+    const { page, pageSize } = this.props.list;
+    this.props.fetchList(page, pageSize, keyword);
+  };
+  handlePageChange = newPage => {
+    this.props.history.push(`/list-page/${newPage}`);
+    // this.props.fetchList(newPage);
+  };
+  render() {
+    if (this.props.list.fetchListError) {
+      return <div>{this.props.list.fetchListError.error.message}</div>;
+    }
+    if (!this.props.list.items || !this.props.list.items.length)
+      return "loading...";
+    const { page, total, pageSize, keyword } = this.props.list;
+    return (
+      <div>
+        <h2>User List</h2>
+        <Input.Search
+          value={this.state.search}
+          onChange={e => this.setState({ search: e.target.value })}
+          style={{ width: "200px" }}
+          onSearch={this.handleSearch}
+        />
+        <br />
+        <br />
+        <Table
+          dataSource={this.getDataSource(this.props.list)}
+          columns={this.getColumns()}
+          style={{ width: "400px" }}
+          rowKey="id"
+          loading={this.props.list.fetchListPending}
+          pagination={false}
+        />
+        <br />
+        <Pagination
+          current={page}
+          onChange={this.handlePageChange}
+          total={total}
+          pageSize={pageSize}
+        />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    list: state,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchList }, dispatch);
+}
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(ListPage),
+);
---
+++ src/c33/MultipleRequest.js
@@ -0,0 +1,176 @@
+import React, { Component } from "react";
+import _ from "lodash";
+import { Button, DatePicker, Form, Input, Select } from "antd";
+import FormBuilder from "../c29/FormBuilder";
+
+const Option = Select.Option;
+
+const fetchUserInfo = id => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        userName: "Nate",
+        province: "shanghai",
+        city: "pudong",
+      });
+    }, 1000);
+  });
+};
+
+const fetchProvices = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        { name: "北京", key: "beijing" },
+        { name: "上海", key: "shanghai" },
+        { name: "江苏", key: "jiangsu" },
+        { name: "山东", key: "shandong" },
+      ]);
+    }, 1000);
+  });
+};
+
+const fetchCities = province => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(
+        {
+          beijing: [
+            { name: "朝阳", key: "chaoyang" },
+            { name: "海淀", key: "haidian" },
+          ],
+          shanghai: [
+            { name: "浦东", key: "pudong" },
+            { name: "徐汇", key: "xuhui" },
+          ],
+          jiangsu: [
+            { name: "南京", key: "nanjing" },
+            { name: "苏州", key: "suzhou" },
+          ],
+          shandong: [
+            { name: "青岛", key: "qingdao" },
+            { name: "德州", key: "dezhou" },
+          ],
+        }[province],
+      );
+    }, 1000);
+  });
+};
+
+const formMeta = {
+  colon: true,
+  columns: 1,
+  elements: [
+    {
+      key: "userName",
+      label: "Name",
+      tooltip: "user name",
+      widget: Input,
+      required: true,
+    },
+    {
+      key: "province",
+      label: "Province",
+    },
+    {
+      key: "city",
+      label: "City",
+    },
+  ],
+};
+
+class MultipleRequest extends Component {
+  state = {
+    userData: null,
+    provinces: null,
+    cities: null,
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetchProvices().then(data => this.setState({ provinces: data }));
+    fetchUserInfo().then(data => {
+      this.setState({ userData: data });
+      fetchCities(data.province).then(cities => this.setState({ cities }));
+    });
+  }
+
+  getMeta() {
+    const { userData } = this.state;
+    return {
+      ...formMeta,
+      elements: _.compact(
+        formMeta.elements.map(m => {
+          if (m.key === "province") {
+            m = {
+              ...m,
+              widgetProps: {
+                onChange: this.handleProvinceChange,
+              },
+            };
+          }
+          if (m.key === "province" || m.key === "city") {
+            const values = this.state[
+              m.key === "province" ? "provinces" : "cities"
+            ];
+            return {
+              ...m,
+              widget: Select,
+              initialValue:
+                values && userData && userData[m.key]
+                  ? userData[m.key]
+                  : "loading",
+              children: values
+                ? values.map(p => <Option key={p.key}>{p.name}</Option>)
+                : [<Option key="loading">Loading...</Option>],
+            };
+          }
+          return {
+            ...m,
+            initialValue: userData ? userData[m.key] : "",
+          };
+        }),
+      ),
+    };
+  }
+
+  handleProvinceChange = newProvince => {
+    this.setState({ cities: null });
+    fetchCities(newProvince).then(cities =>
+      this.setState(
+        {
+          cities,
+          userData: {
+            ...this.state.userData,
+            city: cities[0].key,
+          },
+        },
+        () => {
+          this.props.form.setFieldsValue({ city: cities[0].key });
+        },
+      ),
+    );
+  };
+
+  render() {
+    return (
+      <Form
+        onSubmit={this.handleSubmit}
+        layout="horizontal"
+        style={{ width: "400px" }}
+      >
+        <FormBuilder meta={this.getMeta()} form={this.props.form} />
+        <div style={{ textAlign: "center" }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    );
+  }
+}
+
+export default Form.create()(MultipleRequest);
